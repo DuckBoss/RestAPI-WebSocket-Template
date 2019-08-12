1c0adebc2b49cdb800b700b3d673874157be0d63
Jason <jasonjerome16@gmail.com>
2019-08-12 16:25:42 +0530

    Added web socket connectivity

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
 const bodyParser = require('body-parser');
-
+const io = require('socket.io')(server);
 
 app.use(bodyParser.json({limit: '150mb'}));
 
@@ -24,6 +25,24 @@ app.delete('/deleteJSON', function (req, res) {
    res.json({"result": "OK"})
 });
 
-const server = app.listen(8081, function () {
+io.on('connection', function(socket) {
+    console.log('A client connected');
+    socket.join('main');
+    console.log('client joined main room');
+
+    socket.on('setJSON', function(data) {
+        curJSON = JSON.parse(data);
+    });
+    socket.on('getJSON', function() {
+        socket.emit('JSONretrieved', curJSON);
+    });
+    socket.on('update-value', (data) => {
+        curJSON = JSON.parse(data);
+        console.log(curJSON);
+        io.sockets.emit('done-updating', data);
+    });
+});
+
+server.listen(8081, () => {
 
 });
